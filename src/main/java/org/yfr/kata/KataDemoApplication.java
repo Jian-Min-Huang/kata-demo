1cc21ffb456ec38209849e456aaa35a575faf25f
Huang Jian-Min <yfr.huang@hotmail.com>
2020-04-25 01:00:14 +0800

    feat: init commit

---
+++ src/main/java/org/yfr/kata/KataDemoApplication.java
@@ -0,0 +1,13 @@
+package org.yfr.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yfr/kata/KataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yfr.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
