015f7656f077ad809ca7a55e55bac12670d2abc3
Huang Jian-Min <yfr.huang@hotmail.com>
2021-02-21 11:45:47 +0800

    add kotlin minimal dependency

--- kata-lab-kotlin/build.gradle.kts
+++ kata-lab-kotlin/build.gradle.kts
@@ -1,10 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.3"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.4.30"
-    kotlin("plugin.spring") version "1.4.30"
 }
 
 group = "com.example"
@@ -16,10 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.10.6")
 }
 
 tasks.withType<KotlinCompile> {
