8899ac0a0f4427e03c8eff8bdd71371c89648ec9
Huang Jian-Min <yfr.huang@hotmail.com>
2021-02-21 14:53:07 +0800

    add kotlin minimal dependency again

--- kata-lab-kotlin/build.gradle.kts
+++ kata-lab-kotlin/build.gradle.kts
@@ -1,19 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val mockkVersion = "1.10.6"
+val junitVersion = "5.7.1"
+
 plugins {
     kotlin("jvm") version "1.4.30"
 }
-
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
+
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
 }
-
 dependencies {
-    testImplementation("io.mockk:mockk:1.10.6")
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 tasks.withType<KotlinCompile> {
